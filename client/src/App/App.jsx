d7e2cda0bd9616234b8d7e7c601a49b89e75f219
unknown <mouradrtibi9@gmail.com>
2023-06-17 19:02:36 +0200

    first commit

---
+++ client/src/App/App.jsx
@@ -0,0 +1,48 @@
+import { useContext, useState } from 'react'
+import PostDetails from '../components/PostDetails/PostDetails'
+import Navbar from '../components/Navbar/Navbar'
+import FloatingNavbar from '../components/Navbar/FloatingNavbar'
+import Home from '../components/Home'
+import Auth from '../components/Auth/Auth'
+import UserDetails from '../components/User/Details'
+import UserUpdate from '../components/User/Update'
+import SnackBar from '../components/SnackBar'
+import ScrollToTop from '../components/ScrollToTop'
+import ForgotPassword from '../components/Auth/ForgotPassword'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { SnackbarContext } from '../contexts/SnackbarContext'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { classes, Root } from './styles'
+
+const App = () => {
+	const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
+	const { snackBarProps } = useContext(SnackbarContext)
+
+	return (
+		<GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+			<BrowserRouter>
+				<Root className={classes.root}>
+					<div className={classes.blur}>
+							<Navbar user={user} setUser={setUser} />
+							<FloatingNavbar user={user} setUser={setUser} />
+							<Routes>
+								<Route path="/" element={<Navigate to="/posts" />} />
+								<Route path="/posts" element={<Home user={user} />} />
+								<Route path="/posts/search" element={<Home user={user} />} />
+								<Route path="/posts/:id" element={<PostDetails user={user} />} />
+								<Route path="/user" element={user ? <UserDetails user={user} /> : <Navigate to="/" />} />
+								<Route path="/user/update" element={user ? <UserUpdate user={user} setUser={setUser} /> : <Navigate to="/" />} />
+								<Route path="/auth" element={user ? <Navigate to="/" /> : <Auth />} />
+								<Route path="/auth/forgotPassword" element={user ? <Navigate to="/" /> : <ForgotPassword />} />
+								<Route path="/auth/forgotPassword/:id/:token" element={user ? <Navigate to="/" /> : <ForgotPassword />} />
+							</Routes>
+							<ScrollToTop />
+							<SnackBar {...snackBarProps} />
+					</div>
+				</Root>
+			</BrowserRouter>
+		</GoogleOAuthProvider>
+	)
+}
+
+export default App
---
+++ server/controllers/posts.js
@@ -0,0 +1,177 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Post from '../models/post.js'
+import Media from '../models/media.js'
+
+const router = express.Router()
+
+export const setCreator = (posts) =>
+	posts.map((post) => {
+		delete post.creator[0].password
+		delete post.creator[0].resetToken
+		return { ...post, creator: post.creator[0] }
+	})
+
+export const getPosts = async (req, res) => {
+	const { page } = req.query
+
+	try {
+		const userId = mongoose.Types.ObjectId(req.userId?.padStart(24, '0'))
+		const query = { $or: [{ private: false }, { creator: userId }] }
+		const LIMIT = 8
+		const total = await Post.countDocuments(query)
+		const startIndex = (Number(page) - 1) * LIMIT // get the starting index of every page
+		console.log('Fetching posts')
+		const start = Date.now()
+		let posts = await Post.aggregate([
+			{ $match: query },
+			{ $sort: { createdAt: -1 } },
+			{ $skip: startIndex },
+			{ $limit: LIMIT },
+			{
+				$lookup: {
+					from: 'users',
+					localField: 'creator',
+					foreignField: '_id',
+					as: 'creator',
+				},
+			},
+		])
+		posts = setCreator(posts)
+		const end = Date.now()
+		console.log(`Fetching took ${(end - start) / 1000} seconds`)
+		res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) })
+	} catch (error) {
+		console.log(error)
+		res.status(404).json({ message: error.message })
+	}
+}
+
+export const getPost = async (req, res) => {
+	const { id } = req.params
+	try {
+		if (!mongoose.Types.ObjectId.isValid(id)) {
+			return res.status(404).send('Invalid Id. Post Not Found')
+		}
+		let posts = await Post.aggregate([
+			{ $match: { _id: mongoose.Types.ObjectId(id) } },
+			{
+				$lookup: {
+					from: 'users',
+					localField: 'creator',
+					foreignField: '_id',
+					as: 'creator',
+				},
+			},
+			{
+				$lookup: {
+					from: 'media',
+					localField: '_id',
+					foreignField: '_id',
+					as: 'media',
+				},
+			},
+			{
+				$addFields: {
+					image: { $arrayElemAt: ['$media.image', 0] },
+				},
+			},
+			{
+				$project: {
+					thumbnail: 0,
+					media: 0,
+				},
+			},
+		])
+
+		posts = setCreator(posts)
+		if (!posts.length) {
+			return res.status(404).send('Post not found with that id')
+		}
+		const post = posts[0]
+		if (post.private && `${post.creator._id}` !== req.userId?.padStart(24, 0)) {
+			return res.status(404).send("Can't access post with that Id")
+		}
+		res.status(200).json(post)
+	} catch (error) {
+		console.log(error)
+		res.status(404).json({ message: error.message })
+	}
+}
+
+export const getPostsBySearch = async (req, res) => {
+	const { searchQuery, tags } = req.query
+	try {
+		const title = new RegExp(searchQuery, 'i')
+		const userId = mongoose.Types.ObjectId(req.userId?.padStart(24, '0'))
+		const query = {
+			$and: [
+				{
+					$or: [{ creator: userId }, { private: false }],
+				},
+				{
+					$or: [{ title }, { tags: { $in: tags.split(',') } }],
+				},
+			],
+		}
+		let posts = await Post.aggregate([
+			{ $match: query },
+			{ $sort: { createdAt: -1 } },
+			{
+				$lookup: {
+					from: 'users',
+					localField: 'creator',
+					foreignField: '_id',
+					as: 'creator',
+				},
+			},
+		])
+		posts = setCreator(posts)
+		res.status(200).json({ data: posts })
+	} catch (error) {
+		res.status(404).json({ message: error.message })
+	}
+}
+
+export const createPost = async (req, res) => {
+	const post = req.body
+	const media = post.image
+	try {
+		const newPost = new Post({
+			...post,
+			creator: mongoose.Types.ObjectId(req.userId.padStart(24, '0')),
+		})
+		await newPost.save()
+
+		const newMedia = new Media({
+			_id: newPost._id,
+			image: media,
+		})
+		await newMedia.save()
+		res.status(201).json(newPost)
+	} catch (error) {
+		res.status(409).send(error.message)
+	}
+}
+
+export const updatePost = async (req, res) => {
+	const { id: _id } = req.params
+	if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+
+	const post = req.body
+	const updatedPost = await Post.findByIdAndUpdate(_id, { ...post, _id }, { new: true })
+	await Media.findByIdAndUpdate(_id, { image: post.image })
+	res.status(200).json(updatedPost)
+}
+
+export const deletePost = async (req, res) => {
+	const { id } = req.params
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).send('No post with that id')
+	}
+	await Post.findByIdAndRemove(id)
+	await Media.findByIdAndRemove(id)
+	res.json({ message: 'Post deleted Successfully' })
+}
+
+export default router
